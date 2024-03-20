0aefc52686973e8d2c256dcdc6c1e8f750ab49f3
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2024-03-21 10:57:09 +1100

    Fleshed out OCR function. Ready to test in deployment

--- src/op_ocr.py
+++ src/op_ocr.py
@@ -1,7 +1,7 @@
 import logging
 import pytesseract
 
-from .shared_azure_dl import read_image_from_data_lake
+from .shared_azure_dl import read_image_from_data_lake, upload_dataframe_to_data_lake
 
 
 def ocr_image(file_path: str, file_id: str, lang="eng"):
@@ -10,6 +10,8 @@ def ocr_image(file_path: str, file_id: str, lang="eng"):
 
     image = read_image_from_data_lake(file_path)
 
-    result = pytesseract.image_to_data(image, lang=lang)
+    result_df = pytesseract.image_to_data(image, lang=lang, output_type="data.frame")
 
-    return result
+    uploaded = upload_dataframe_to_data_lake("ocr", result_df, file_id)
+
+    return uploaded
--- src/shared_azure_dl.py
+++ src/shared_azure_dl.py
@@ -2,6 +2,7 @@ import logging
 import os
 import io
 from PIL import Image
+import pandas as pd
 
 from azure.storage.filedatalake import DataLakeFileClient, DataLakeServiceClient
 
@@ -43,8 +44,48 @@ def read_image_from_data_lake(file_path: str, file_system="raw"):
         return False
 
 
-# upload to dl
-# sample_filepath = "image-pipeline/image-capture/018/WIN_20240318_09_50_20_Pro.jpg"
+def upload_to_data_lake(file_system: str, file_path: str, file_contents: str) -> bool:
+    connection_str = os.environ.get("DATA_LAKE_CONNECTION_STRING")
+    logging.info(f"Adding blob: {file_path} to: {file_system}")
 
-# dl_image = read_image_from_data_lake(sample_filepath)
-# dl_image.show()
+    try:
+        file_client = DataLakeFileClient.from_connection_string(
+            connection_str, file_system, file_path, connection_timeout=14400
+        )
+
+        file_client.create_file()
+        file_client.append_data(file_contents, offset=0, length=len(file_contents))
+        file_client.flush_data(len(file_contents))
+        logging.info(f"{file_path} uploaded successfully to {file_system}")
+
+        return True
+
+    except Exception as e:
+        logging.error(
+            f"An error occurred when trying to create file: {file_path}. Here's the error: {e}"
+        )
+        return False
+
+
+def upload_dataframe_to_data_lake(
+    operation: str,
+    dataframe: pd.DataFrame,
+    file_id: str,
+    delimiter=",",
+    file_system="raw",
+    pipeline="image-pipeline",
+    file_suffix="txt",
+):
+    file_path = f"{pipeline}/{operation}/{file_id}.{file_suffix}"
+
+    content_list = dataframe.values.tolist()
+
+    # if columns_supplied:
+    #     content_list.insert(0, list(dataframe.columns))
+
+    file_contents = [f"{delimiter}".join(map(str, content)) for content in content_list]
+    file_contents = "\n".join(file_contents)
+
+    uploaded = upload_to_data_lake(file_system, file_path, file_contents)
+
+    return uploaded
