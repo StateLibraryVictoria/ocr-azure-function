7bc74e663f090379296f8cdbeee50b26eef7568b
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2024-03-21 12:09:15 +1100

    Added placeholder ner function. Trying to trick this into working

--- function_app.py
+++ function_app.py
@@ -1,20 +1,20 @@
 import azure.functions as func
 import logging
 
-from despatch_job import despatch_job
+# from despatch_job import despatch_job
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.route(route="ocr", auth_level=func.AuthLevel.FUNCTION)
-def ocr(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("OCR function invoked")
-    try:
-        response = despatch_job(req)
-        return response
+# @app.route(route="ocr", auth_level=func.AuthLevel.FUNCTION)
+# def ocr(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info("OCR function invoked")
+#     try:
+#         response = despatch_job(req)
+#         return response
 
-    except Exception as e:
-        logging.error("An error occurred invoking OCR operation: {e}")
+#     except Exception as e:
+#         logging.error("An error occurred invoking OCR operation: {e}")
 
 
 @app.route(route="named_entity_recognition", auth_level=func.AuthLevel.FUNCTION)
