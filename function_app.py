067be71bd634c1f659dd76424adacb240d647e1c
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2024-03-21 13:47:43 +1100

    re-instated OCR logic

--- function_app.py
+++ function_app.py
@@ -1,20 +1,20 @@
 import azure.functions as func
 import logging
 
-# from despatch_job import despatch_job
+from src.despatch_job import despatch_job
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-# @app.route(route="ocr", auth_level=func.AuthLevel.FUNCTION)
-# def ocr(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info("OCR function invoked")
-#     try:
-#         response = despatch_job(req)
-#         return response
+@app.route(route="ocr", auth_level=func.AuthLevel.FUNCTION)
+def ocr(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("OCR function invoked")
+    try:
+        response = despatch_job(req)
+        return response
 
-#     except Exception as e:
-#         logging.error("An error occurred invoking OCR operation: {e}")
+    except Exception as e:
+        logging.error("An error occurred invoking OCR operation: {e}")
 
 
 @app.route(route="named_entity_recognition", auth_level=func.AuthLevel.FUNCTION)
@@ -23,25 +23,3 @@ def named_entity_recognition(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("NER placeholder")
 
     return func.HttpResponse("True")
-
-
-@app.route(route="ocr", auth_level=func.AuthLevel.FUNCTION)
-def ocr(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
