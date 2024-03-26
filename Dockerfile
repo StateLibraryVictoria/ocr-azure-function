fa89b743285bba104808a72dee952246db93e4d1
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2024-03-26 16:17:53 +1100

    Moving to using a durable function for NER function

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,4 @@ RUN pip install -r /requirements.txt
 RUN apt-get update && apt-get install -y \
 tesseract-ocr
 
-ADD download_models.py /home/download_models.py
-RUN python /home/download_models.py
-
 COPY . /home/site/wwwroot
--- function_app.py
+++ function_app.py
@@ -1,9 +1,13 @@
 import azure.functions as func
-import logging
+import azure.durable_functions as df
 
+import logging
 
 from src import despatch_job
 
+
+# function 1 call OCR on all files
+dfApp = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
@@ -22,17 +26,26 @@ def ocr(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(error_msg)
 
 
-@app.route(route="ner", auth_level=func.AuthLevel.ANONYMOUS)
-def ner(req: func.HttpRequest) -> func.HttpResponse:
+# durable function 2 load model and call NER on all files
+@dfApp.route(route="orchestrators/{functionName}")
+@dfApp.durable_client_input(client_name="client")
+async def http_start(req: func.HttpRequest, client):
+    function_name = req.route_params.get("functionName")
+    instance_id = await client.start_new(function_name)
+    response = client.create_check_status_response(req, instance_id)
+    return response
 
-    logging.info("NER function invoked")
 
-    try:
-        response = despatch_job.despatch_job(req)
-        return response
+@dfApp.orchestration_trigger(context_name="context")
+def hello_orchestrator(context):
 
-    except Exception as e:
-        error_msg = f"Failed: NER operation error {e}"
-        logging.error(error_msg)
+    result1 = yield context.call_activity("hello", "Seattle")
+    result2 = yield context.call_activity("hello", "Tokyo")
+    result3 = yield context.call_activity("hello", "London")
 
-        return func.HttpResponse(error_msg)
+    return [result1, result2, result3]
+
+
+@dfApp.activity_trigger(input_name="city")
+def hello(city: str):
+    return f"Hello {city}"
